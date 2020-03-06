d7768a506e3f6cfbaa92376cff11669580312947
robertd2719 <45602920+robertd2719@users.noreply.github.com>
2020-03-06 14:55:50 -0500

    testCommit

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 const express = require('express');
 // we also need to require the 'path' module
 const path = require('path');
+// this is where we are going to import ejs module
+const ejs = require('ejs')
 
 app = new express();
+
+//set our view engine
+app.set('view engine','ejs');
+
+//middleware for static and rendering pages
 app.use(express.static('public'));
 
 //create actual routes to our pages:
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'pages/index.html'));
+    res.render('index');
 });
 
 app.get('/about',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'pages/about.html'));
+   res.render('about')
 });
 
 app.get('/contact',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'pages/contact.html'));
+    res.render('contact')
 });
-
 app.get('/post',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'pages/post.html'))
+    res.render('post')
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT ||5000;
 app.listen(PORT,()=>{
     console.log(`Server started on port: ${PORT}`);
 });
