4000b9e07d98d60fa9c7965427611c62c3b77e13
robertd2719 <45602920+robertd2719@users.noreply.github.com>
2020-03-06 14:40:35 -0500

    added the new relavtive links of / in place of index.html for all .html pages

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require('express');
+// we also need to require the 'path' module
+const path = require('path');
 
 app = new express();
 app.use(express.static('public'));
+
+//create actual routes to our pages:
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'pages/index.html'));
+});
+
+app.get('/about',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'pages/about.html'));
+});
+
+app.get('/contact',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'pages/contact.html'));
+});
+
+app.get('/post',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'pages/post.html'))
+});
 
 const PORT = 5000;
 app.listen(PORT,()=>{
